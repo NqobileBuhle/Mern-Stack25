ba564a1150aa76adc156d495f25a8804cd5b5d9f
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2025-01-21 12:07:58 +0200

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,16 @@
 import express from "express";
-import Product from "./models/product.model.js";
-
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/products.routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON data from the request body
 
-app.get("/api/products",async (req,res)=>{
-    try {
-        const products=await Product.find({});
-        res.status(200).json({success:true,data:products});
-    } catch (error) {
-        console.log("erroe fetching products:",error.message);
-        res.status(500).json({success:false,message:"Server error"});
-    }
-})
-
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // Correctly accessing the request body
+app.use("/api/products",productRoutes)
 
 
-  // Validate the incoming product data
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in creating product:", error.message);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-});
-app.put("/api/products/:id",async(req,res)=>{
-    const {id}=req.params;
-    const product=req.body;
-
-    try {
-        const updatedProduct=await Product.findByIdAndUpdate(id,product,{new:true});
-        res.status(200).json({success:true,data:updatedProduct});
-    
-    } catch (error) {
-        res.status(500).json({success:false,message:"Server error"});
-    }
-})
-app.delete("/api/products/:id",async (req,res)=>{
-    const {id}=req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success:true,message:"Product deleted"});
-    } catch (error) {
-        res.status(400).json({success:false,message:"Product not found"});
-    }
-});
 
 app.listen(5000, () => {
   connectDB();
